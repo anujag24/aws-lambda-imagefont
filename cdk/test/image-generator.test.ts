f75ef77739a2a20e7499edfa90f582b16b987f1e
Anuj Gupta <37851205+anujag24@users.noreply.github.com>
2025-04-18 14:02:21 -0700

    Update image-generator.test.ts

--- cdk/test/image-generator.test.ts
+++ cdk/test/image-generator.test.ts
@@ -2,32 +2,35 @@ import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
 import * as ImageGenerator from '../lib/image-generator-stack';
 
-test('Stack Creates Required Resources', () => {
-  const app = new cdk.App();
-  const stack = new ImageGenerator.ImageGeneratorStack(app, 'TestStack');
-  const template = Template.fromStack(stack);
+describe('ImageGeneratorStack', () => {
+  test('Stack Creates Required Resources', () => {
+    const app = new cdk.App();
+    const stack = new ImageGenerator.ImageGeneratorStack(app, 'TestStack', {
+      env: {
+        account: '123456789012',
+        region: 'us-west-2',
+      },
+      pillowLayerArn: 'arn:aws:lambda:us-west-2:770693421928:layer:Klayers-p312-Pillow:5'
+    });
+    
+    const template = Template.fromStack(stack);
 
-  // Test S3 Bucket
-  template.hasResourceProperties('AWS::S3::Bucket', {
-    VersioningConfiguration: {
-      Status: 'Enabled'
-    }
-  });
+    template.hasResourceProperties('AWS::S3::Bucket', {
+      VersioningConfiguration: {
+        Status: 'Enabled'
+      }
+    });
 
-  // Test Font Layer
-  template.hasResourceProperties('AWS::Lambda::LayerVersion', {
-    Description: 'Font configuration and DejaVu Sans font',
-    CompatibleRuntimes: ['python3.12']
-  });
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      Description: 'Font configuration and DejaVu Sans font',
+      CompatibleRuntimes: ['python3.12']
+    });
 
-  // Test Lambda Function
-  template.hasResourceProperties('AWS::Lambda::Function', {
-    Runtime: 'python3.12',
-    Handler: 'lambda_function.lambda_handler',
-    Timeout: 30,
-    MemorySize: 256,
-    Layers: Match.arrayWith([
-      Match.stringLikeRegexp('arn:aws:lambda:.*:layer:Klayers-p312-Pillow:5')
-    ])
+    template.hasResourceProperties('AWS::Lambda::Function', {
+      Runtime: 'python3.12',
+      Handler: 'lambda_function.lambda_handler',
+      Timeout: 30,
+      MemorySize: 256
+    });
   });
 });
